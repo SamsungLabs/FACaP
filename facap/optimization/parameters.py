bcf4dcf8380aae8505bf156e20017baf2fda09e4
Filipp Nikitin <f.nikitin@samsung.com>
2022-03-30 14:11:05 +0300

    solved issue with speed (cache indexes)

--- facap/optimization/parameters.py
+++ facap/optimization/parameters.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 
 from facap.data.scan import Camera
+from collections import OrderedDict
 
 
 class CameraParameters(nn.Module):
@@ -16,6 +17,8 @@ class CameraParameters(nn.Module):
         pp = torch.stack([torch.tensor(cameras[i].pp) for i in cameras])
         self.register_buffer("f", f)
         self.register_buffer("pp", pp)
+        self.cached_indexes = OrderedDict()
+        self.n_cached = 5
 
     def get_cameras(self):
         cameras = {}
@@ -31,9 +34,20 @@ class CameraParameters(nn.Module):
         return cameras
 
     def get_tensors(self, camera_idxs):
-        idxs = [self.cam2id[i] for i in camera_idxs]
+        idxs = self.get_cached_indexes(camera_idxs)
         rotvecs = self.rotvecs[idxs]
         translations = self.translations[idxs]
         f = self.f[idxs]
         pp = self.pp[idxs]
         return rotvecs, translations, f, pp
+
+    def get_cached_indexes(self, cameras):
+        for key in self.cached_indexes:
+            if key == id(cameras):
+                return self.cached_indexes[key]
+        if len(self.cached_indexes) == self.n_cached:
+            self.cached_indexes.popitem(last=True)
+        idxs = [self.cam2id[i] for i in cameras]
+        self.cached_indexes.update({id(cameras): idxs})
+        self.cached_indexes.move_to_end(id(cameras), last=False)
+        return idxs
